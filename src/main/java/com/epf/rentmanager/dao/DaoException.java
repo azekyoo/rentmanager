cf725fa0850196fee1e891a3d24a370bfd93422f
carotterapee01 <victor.gaspard@epfedu.fr>
2024-02-13 00:32:39 +0900

    DaoException

---
+++ src/main/java/com/epf/rentmanager/dao/DaoException.java
@@ -0,0 +1,12 @@
+package com.epf.rentmanager.dao;
+
+public class DaoException extends Exception {
+
+     public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
